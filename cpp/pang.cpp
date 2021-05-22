69deab68ca697218f1780a710bd5a77e2e73a720
Ankur Seth <iankurseth@gmail.com>
2021-05-22 21:13:02 +0530

    renamed cpp project to pang

--- cpp/pang.cpp
+++ cpp/pang.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-#include "squares/resolution.hpp"
+#include "pang/resolution.hpp"
 
 int main (int argc, char const *argv[])
 {
-	std::cout << "Hello, squares!\n";
+	std::cout << "Hello, pang!\n";
 	return 0;
 }
