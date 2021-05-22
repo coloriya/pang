11c550284604efe0f3a33de9e7091a158a84b0b8
Ankur Seth <iankurseth@gmail.com>
2021-05-22 18:37:41 +0530

    now allowing for cmd command and args

--- app.js
+++ app.js
@@ -2,8 +2,22 @@
 const PalpngApp = require("./palpng");
 const app = new PalpngApp();
 
-app.consoleLog();
-app.saveCss();
+const argv = process.argv;
+const command = argv[2] ? argv[2].toLowerCase() : null;
+const arg = argv[3] ? argv[3].toLowerCase() : null;
+
+switch (command) {
+	case "log":
+		app.consoleLog();
+		break;
+	case "css":
+		app.saveCss();
+		break;
+	default:
+		console.log(`Unknown command: '${command}'`);
+}
+
+
 
 process.exit();
 
