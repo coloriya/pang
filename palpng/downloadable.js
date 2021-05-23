02ceb26e5abf6d6c86c2a54699133611af12709d
Ankur Seth <iankurseth@gmail.com>
2021-05-23 19:30:19 +0530

    added basic getters to PangDownloadable and PangResolution

--- palpng/downloadable.js
+++ palpng/downloadable.js
@@ -7,6 +7,18 @@ const path = require("path");
 function PangDownloadable (app, json) {
 	this.app = app;
 	this.json = json;
+	this.name = this.json.name;
+	this.title = this.json.title;
+}
+
+
+
+PangDownloadable.prototype.getName = function () {
+	return this.name;
+}
+
+PangDownloadable.prototype.getTitle = function () {
+	return this.title;
 }
 
 
--- palpng/resolution.js
+++ palpng/resolution.js
@@ -4,6 +4,23 @@
 function PangResolution (app, json) {
 	this.app = app;
 	this.json = json;
+	this.name = this.json.name;
+	this.height = this.json.height;
+	this.width = this.json.width;
+}
+
+
+
+PangResolution.prototype.getName = function () {
+	return this.name;
+}
+
+PangResolution.prototype.getHeight = function () {
+	return this.height;
+}
+
+PangResolution.prototype.getWidth = function () {
+	return this.width;
 }
 
 
